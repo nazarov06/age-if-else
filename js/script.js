60016d4ec31621b1ee21b83037ba80a6ba4da8fc
Ozodbek Nazarov <asattorov712@gmail.com>
2023-01-20 17:44:43 +0500

    hw2

--- js/script.js
+++ js/script.js
@@ -1,45 +1,28 @@
-// let userName = prompt('Ismingiz')
-// let age = +prompt('Yoshingiz')
+let userName = prompt('Ismingiz')
+let age = +prompt('Yoshingiz')
 
-// let userName1 = prompt('Ismingiz')
-// let age1 = +prompt('Yoshingiz')
+let userName1 = prompt('Ismingiz')
+let age1 = +prompt('Yoshingiz')
 
-// let user1 = {
-//     name: userName,
-//     age: age,
-// }
+let user1 = {
+    name: userName,
+    age: age
+}
 
-// let user2 = {
-//     name: userName1,
-//     age: age1
-// }
+let user2 = {
+    name: userName1,
+    age: age1
+}
 
-// if (user1.age > user2.age) {
-//     alert(`${user1.name} ${user2.name} dan katta`)
-// } else if (user1.age < user2.age) {
-//     alert(`${user1.name} ${user2.name} dan kichkina`)
-// } else if (user1.age == user2.age) {
-//     alert(`${user1.name} va ${user2.name} ning yoshlari teng`)
-// } else {
-//     alert('Error')
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if (user1.age > user2.age) {
+    alert("Birinchi  Ikkinchidan katta")gi
+} else if (user1.age < user2.age) {
+    alert("Birinchi Ikkinchidan kichkina")
+} else if (user1.age == user2.age) {
+    alert( "Birinchi va Ikkinchini ning yoshlari teng")
+} else {
+    alert('Error')
+}
 
 
 
@@ -47,7 +30,7 @@ let firstNumber = +prompt('1-sonni kiriting')
 let operator = prompt('Matematik amalni kiriting (+ - * /)')
 let secondNumber = +prompt('2-sonni kiriting')
 
-if (!isNaN(Number(firstNumber)) && !isNaN(Number(secondNumber))) {
+if (firstNumber && secondNumber) {
     if (operator === '+') {
         alert(firstNumber + secondNumber);
     } else if (operator === '-') {
